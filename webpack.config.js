ecb511736aee84a1c84a91ef70cc6411981a1267
Kevin Ostafinski <kevscripted@gmail.com>
2021-06-18 00:23:08 +0200

    updated webpack config + prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,48 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   module: {
     rules: [
-      { test: /\.(scss|css)$/, use: ["style-loader", "css-loader", "sass-loader"] },
-      { test: /\.(js)$/, use: "babel-loader" }
-    ]
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      { test: /\.(js)$/, use: "babel-loader" },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: "file-loader",
+        options: {
+          name: "/assets/[name].[ext]",
+        },
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "assets/fonts/[name].[hash].[ext]",
+          },
+        },
+      },
+    ],
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
-      template: path.join(__dirname, "src/index.html")
-    })
+      template: path.join(__dirname, "src/index.html"),
+    }),
   ],
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     port: 3000,
-    open: true
-  }
-
-}
+    open: true,
+  },
+};
