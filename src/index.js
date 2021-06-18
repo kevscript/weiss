1d6f112836aea07559243fd46bc61622154893e6
Kevin Ostafinski <kevscripted@gmail.com>
2021-06-18 17:00:16 +0200

    added assets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import "./styles/main.scss";
 import "./assets/logo.svg";
 import "./assets/mail.svg";
+import "./assets/triangle.svg";
+import "./assets/arrow.svg";
+import "./assets/hero.jpg";
+import "./assets/item1.jpg";
+import "./assets/item2.jpg";
+import "./assets/item3.jpg";
