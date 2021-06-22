6443c7ddf6453e416900f75eaed8c749ae39e128
Kevin Ostafinski <kevscripted@gmail.com>
2021-06-22 22:26:01 +0200

    reconfigured webpack structure and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,36 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const plugins = require("./webpack.plugins");
+const loaders = require("./webpack.loaders");
+
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production"
+}
+
 
 module.exports = {
+  mode: mode,
+  target: "web",
   entry: "./src/js/index.js",
   module: {
-    rules: [
-      {
-        test: /\.(scss|css)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      { test: /\.(js)$/, use: "babel-loader" },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loader: "file-loader",
-        options: {
-          name: "/assets/[name].[ext]",
-        },
-      },
-      {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "assets/fonts/[name].[hash].[ext]",
-          },
-        },
-      },
-    ],
+    rules: loaders
   },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     clean: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.join(__dirname, "src/index.html"),
-    }),
-  ],
-  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  plugins,
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()]
+  },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, "src"),
     port: 3000,
     open: true,
+    hot: true
   },
+  devtool: "source-map"
 };
---
+++ webpack.loaders.js
@@ -0,0 +1,44 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const styleLoaders = {
+  test: /\.(scss|css)$/,
+  use: [
+    MiniCssExtractPlugin.loader,
+    "css-loader", 
+    "postcss-loader",
+    "sass-loader"
+  ]
+}
+
+const jsLoaders = {
+  test: /\.(js)$/, 
+  exclude: /node_modules/,
+  use: {
+    loader: "babel-loader"
+  }
+}
+
+const imageLoaders = {
+  test: /\.(jpe?g|png|gif|svg)$/i,
+  loader: "file-loader",
+  options: {
+    name: "/assets/[name].[contenthash].[ext]",
+  },
+}
+
+const fontLoaders = {
+  test: /\.(eot|ttf|woff|woff2)$/,
+  use: {
+    loader: "file-loader",
+    options: {
+      name: "assets/fonts/[name].[contenthash].[ext]",
+    },
+  }
+}
+
+module.exports = [
+  styleLoaders,
+  jsLoaders,
+  imageLoaders,
+  fontLoaders
+]
